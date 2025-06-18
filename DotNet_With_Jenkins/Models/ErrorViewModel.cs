ec65372b239a1f3689f5e14d57d4ccd3828abd7f
Jovi Tilroy D'Silva <v-jtilroydsilva@mirion.com>
2025-06-18 15:58:30 +0530

    Add project files.

---
+++ DotNet_With_Jenkins/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNet_With_Jenkins.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
